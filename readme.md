6b7fc2573f92351639ff155e6b9aa571e28e30b7
Kinse <22683778+sofoxe1@users.noreply.github.com>
2024-04-06 18:42:02 +0200

    stuff

--- readme.md
+++ readme.md
@@ -1,17 +1,12 @@
 script to edit config files then reboot  
-config path: /etc/reboot.py/config.ini
-#### config example:
-```
-[General]
-targets=test
-files=/file-to-edit,/etc/default/grub
-```
-#### lines in config file need to end with #reboot-<target> for example:
+before using edit and move `config.ini.example` to `/etc/reboot.py/config.ini` 
+
+#### lines in config files need to end with #reboot-<target> for example:
 ```
 GRUB_HIDDEN_TIMEOUT=0 #reboot-grub_no_wait
 GRUB_HIDDEN_TIMEOUT=5 #reboot-grub_wait
 ```
-will comment out first line (and uncomment second one) when `reboot.py grub_wait` is run 
+script will comment out first line (and uncomment second one) when `reboot.py grub_wait` is run 
 
 #### features:
 target autocompletion, `reboot.py t` will be autocomplete to `reboot.py target`  
@@ -19,9 +14,4 @@ target autocompletion, `reboot.py t` will be autocomplete to `reboot.py target`
 `-g` regenerates grub config  
 `-y` doesnt ask before rebooting  
 
-#### todo
-default behavior for target  
-detect tool used for initrd  
-make it drop replacment for 'reboot'  
-handle not existing config
 
--- reboot.py
+++ reboot.py
@@ -15,7 +15,26 @@ config = configparser.ConfigParser()
 config.read("/etc/reboot.py/config.ini")
 targets=config.get('General','targets').split(",")
 files=config.get('General','files').split(",")
+initrd_command=config.get('General','initrd_command')
+grub_command=config.get('General','grub_command')
+values=list(config.items("General"))
 
+preexec=None
+postexec=None
+default_args=None
+strip_spaces=False
+
+for v in values:
+    if v[0] == "args":
+        default_args=v[1]
+    elif v[0] == "strip_spaces":
+        strip_spaces=bool(v[1])
+    elif v[0] == "preexec":
+        preexec=v[1]
+    elif v[0] == "postexec":
+        postexec=v[1]
+
+_args=None
 if args.target not in targets:
     c=0
     _t=""
@@ -23,29 +42,49 @@ if args.target not in targets:
         if t.startswith(args.target):
             _t=t
             c+=1
-    if c == 0: raise Exception(f"{target} target doesn't exist")
+    if c == 0: raise Exception(f"{target} doesn't exist")
     elif c>1: raise Exception(f"multiple targets match {target}")
-    else: target=_t
-else: target=args.target
-print(f"booting to {target}")
+    else: args.target=_t
+
+if args.target in config.sections():
+    values=list(config.items(args.target))
+    for v in values:
+        if v[0] == "args":
+            if v[0].startswith("+"):
+                _args=default_args+v[1][1:]
+            else:
+                _args=v[1]
+        elif v[0] == "strip_spaces":
+            strip_spaces=bool(v[1])
+else:
+    if default_args is not None: _args=default_args
 
 
+for c in _args:
+    if c=="g": args.grub=True
+    elif c == "i": args.initrd=True
+    elif c == "y": args.y=True
+    
+
+if preexec is not None: os.system(preexec)
 for file in files:
     with open(file, "r+") as f:
         lines = f.readlines()
         for l in range(len(lines)):
-            if lines[l].endswith(f"#reboot-{target}\n") and lines[l].startswith("#"): lines[l]=lines[l][1:] 
-            for _target in [t for t in targets if t!=target]:
-                if lines[l].endswith(f"#reboot-{_target}\n") and not lines[l].startswith("#"): lines[l]="#"+lines[l] 
+            if strip_spaces:
+                lines[l]=lines[l].strip()
+            if lines[l].endswith(f"#reboot-{args.target}\n") and lines[l].startswith("#"): lines[l]=lines[l][1:] 
+            for target in [t for t in targets if t!=args.target]:
+                if lines[l].endswith(f"#reboot-{target}\n") and not lines[l].startswith("#"): lines[l]="#"+lines[l] 
         lines="".join(lines)
         f.seek(0)
         f.write(lines)
 
-if args.initrd:os.system("dracut --regenerate-all -f")
-if args.grub: os.system("grub2-mkconfig -o /boot/grub2/grub.cfg")
+if args.initrd:os.system(initrd_command)
+if args.grub: os.system(grub_command)
 os.system("sync")
 
 if not args.y:
     input("waiting for enter to reboot")
-
+if postexec is not None: os.system(postexec)
 os.system("reboot")
